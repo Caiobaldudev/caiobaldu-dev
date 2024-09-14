3b0e4b2ca984fc0f001b2abfef38153d8fed8e7b
Caio Balduino <caiobalduinodev@hotmail.com>
2024-09-14 17:04:59 -0300

    ✨ feat: outside click application function

---
+++ js/modules/outsideclick.js
@@ -0,0 +1,20 @@
+export default function outsideClick(element, events, callback) {
+  const html = document.documentElement;
+  const outside = "data-outside";
+
+  if (!element.hasAttribute(outside)) {
+    events.forEach((userEvent) => {
+      html.addEventListener(userEvent, handleOutsideClick);
+    });
+    element.setAttribute(outside, "");
+  }
+  function handleOutsideClick(e) {
+    if (!element.contains(e.target)) {
+      element.removeAttribute(outside);
+      events.forEach((userEvent) => {
+        html.removeEventListener(userEvent, handleOutsideClick);
+      });
+      callback();
+    }
+  }
+}
