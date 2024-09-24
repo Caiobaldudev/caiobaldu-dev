0057a805bdcbda4d445e0b5eca9ec7c88a855a4a
Caio Balduino <caiobalduinodev@hotmail.com>
2024-09-24 18:02:01 -0300

    🐛 fix: Resolve bug in mobile menu

--- js/modules/menu-mobile.js
+++ js/modules/menu-mobile.js
@@ -3,16 +3,19 @@ import outsideClick from "./outsideclick.js";
 export default function initMenuMobile() {
   const menuButton = document.querySelector('[data-menu="button"]');
   const menuList = document.querySelector('[data-menu="list"]');
-  const eventos = ["click", "touchstart"];
+  const eventos = ["click"];
 
   if (menuButton) {
     function openMenu() {
-      menuList.classList.add("active");
-      menuButton.classList.add("active");
-      outsideClick(menuList, eventos, () => {
-        menuList.classList.remove("active");
-        menuButton.classList.remove("active");
-      });
+      const isActive = menuList.classList.contains("active");
+      if (!isActive) {
+        menuList.classList.add("active");
+        menuButton.classList.add("active");
+        outsideClick(menuList, eventos, () => {
+          menuList.classList.remove("active");
+          menuButton.classList.remove("active");
+        });
+      }
     }
     eventos.forEach((userEvent) => {
       menuButton.addEventListener(userEvent, openMenu);
